7613cd19409a7a5122afd76b56d460e1c25b9b13
adharshv <51755246+adharshv@users.noreply.github.com>
2019-10-31 20:02:35 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,85 @@
-// client-side javascript
+const path = require('path')
+const express = require('express') 
+const hbs = require('hbs')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
-console.log('Client side js file is loaded.')
+const app = express()
 
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input') // getting the input element
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-weatherForm.addEventListener('submit', (e)=>{ // e for event
-    e.preventDefault() // to prevent the browser from automatically refreshing the page
+// Set up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-    const location = search.value // getting the value put into the input textbox
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = '' //clearing any previous results
 
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
-        response.json().then((data)=>{
-            if(data.error) return messageOne.textContent = data.error
+// Set up static directory to serve
+app.use(express.static(publicDirPath))
 
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-        })
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Adharsh Viswanath'
     })
+})
 
+app.get('/about', (req, res)=> {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Adharsh Viswanath'
+    })
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help', {
+        message: 'Help page.',
+        title: 'Help',
+        name: 'Adharsh Viswanath'
+    })
+})
+
+app.get('/weather', (req, res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: 'Address is required!'
+        })
+    }
+    geocode(req.query.address, (error, {latitude, longitude, location} = {})=>{ // the callback function receives either error or data(destructured)
+        if(error) return res.send({error: error})  
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) return res.send({error: error})
+
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address: req.query.address
+            })
+        })
+    
+    })
+})
+
+app.get('/help/*', (req, res)=>{
+    res.render('404', {
+        message: 'Help article not found!',
+        title: '404',
+        name: 'Adharsh Viswanath'
+    })
+})
+
+app.get('*', (req, res)=>{
+    res.render('404', {
+        message: 'Page not found!',
+        title: '404',
+        name: 'Adharsh Viswanath'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
